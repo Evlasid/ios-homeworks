9f48ac6b3c9daf422ce803b7400acca9a529225c
Евгений Сидоров <EvgenijSidorov@MacBook-Air-Evgeniy-2.local>
2023-05-18 11:52:14 +0300

    finish dev 1 task

---
+++ Novigation/FeedViewController.swift
@@ -0,0 +1,51 @@
+//
+//  FeedViewController.swift
+//  Novigation
+//
+//  Created by Евгений Сидоров on 18.05.2023.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    private lazy var postButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Пост", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemRed
+        
+        return button
+    }()
+    
+    let postName: Post = .init(title: "Пост1")
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            postButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            postButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            postButton.heightAnchor.constraint(equalToConstant: 50),
+            postButton.widthAnchor.constraint(equalToConstant: 150)
+        ])
+    }
+    
+    @objc func showPage () {
+        let postViewController = PostViewController()
+        postViewController.postName = postName.title
+        navigationController?.pushViewController(postViewController, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Feed"
+        postButton.addTarget(self, action: #selector(showPage), for: .touchUpInside)
+        view.addSubview(postButton)
+        setConstraints()
+        
+    }
+    
+
+
+}
---
+++ Novigation/Post.swift
@@ -0,0 +1,14 @@
+//
+//  Post.swift
+//  Novigation
+//
+//  Created by Евгений Сидоров on 18.05.2023.
+//
+
+import UIKit
+
+struct Post {
+    var title: String = ""
+}
+
+let post: Post = .init(title: "Заголовок")
