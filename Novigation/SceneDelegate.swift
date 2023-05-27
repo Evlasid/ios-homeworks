4455dac8a3b9afa25755107c4b30b38e8cb10851
Евгений Сидоров <EvgenijSidorov@MacBook-Air-Evgeniy-2.local>
2023-05-27 15:04:40 +0300

    four commit

--- Novigation/SceneDelegate.swift
+++ Novigation/SceneDelegate.swift
@@ -15,18 +15,29 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let _ = (scene as? UIWindowScene) else { return }
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
+      
         let profileViewcontroller = ProfileViewController()
+
         let profileNovigationViewController = UINavigationController(rootViewController: profileViewcontroller)
         profileNovigationViewController.tabBarItem.title = "Профиль"
         profileNovigationViewController.tabBarItem.image = UIImage(systemName: "person.crop.circle.fill")
 
+        let logInViewController = LogInViewController()
+        let loginNovigationViewController = UINavigationController(rootViewController: logInViewController)
+        loginNovigationViewController.tabBarItem.title = "Профиль"
+        loginNovigationViewController.tabBarItem.image = UIImage(systemName: "person.crop.circle.fill")
+
+       
+        
         
         let feedViewController = FeedViewController()
         let feedNovigationViewController = UINavigationController(rootViewController: feedViewController)
         feedNovigationViewController.tabBarItem.title = "Новости"
         feedNovigationViewController.tabBarItem.image = UIImage(systemName: "note.text")
+        
         let tabViewController = UITabBarController()
-        tabViewController.viewControllers = [profileNovigationViewController, feedNovigationViewController]
+        
+        tabViewController.viewControllers = [loginNovigationViewController, feedNovigationViewController]
         tabViewController.tabBar.backgroundColor = UIColor.systemGray
         tabViewController.tabBar.tintColor = UIColor.systemTeal
         
